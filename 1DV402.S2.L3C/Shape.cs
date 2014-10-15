db1c00ebb02573c868dcec0a44e86d102c82a6f6
Andréas Anemyr <aa223ig@student.lnu.se>
2014-10-15 09:51:45 +0200

    shape.cs och samtliga medlemmar. Dock ingen funktionalitet ännu...

---
+++ 1DV402.S2.L3C/Shape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L3C
+{
+    public abstract class Shape
+    {
+
+        enum ShapeType { Rectangle, Circle, Ellipse, Cuboid, Cylinder, Sphere, };
+
+        //Ska returnera true om figuren är någon av typerna Cuboid, cylinder eller Sphere
+        public bool IsShape3d
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public ShapeType ShapeType{ get; private set; }
+
+        //konstrukorn
+        protected Shape(ShapeType shapeType)
+        {
+            //Tom
+        }
+
+
+        //Ska returnera en formaterad textbeskrivning av objektet
+        public override string ToString(string format)
+        {
+            return base.ToString();
+        }
+
+
+
+
+
+    }
+}
